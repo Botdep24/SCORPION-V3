85289f56c4fa438bd2ae4af81e097211731a6e50
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-10-18 18:30:50 +0200

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -11,7 +11,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `Pong: ${responseTime.toFixed(2)} ms 🎈`;
+    const text = `Pong: ${responseTime.toFixed(2)} ms ✌️`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
